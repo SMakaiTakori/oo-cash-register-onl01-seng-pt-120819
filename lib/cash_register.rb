c3da2db2e269446738ed5be1c76f03c6025e60d0
S. Makai Takori <makait87@gmail.com>
2020-01-25 22:16:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -29,7 +29,7 @@ class CashRegister
   def apply_discount()
     if discount > 0 
       @discount = @discount/100.to_f
-      @total = @total - (@total*@discount)
+      @total = @total-(@total*@discount)
   
     "After the discount, the total comes to $#{@total.to_i}."
     else 
